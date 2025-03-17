3e45442b89da550747dcc182212fe21d86d953f8
mihai-negru <determinant289@gmail.com>
2025-03-17 12:18:05 +0200

    Init projects

--- web-inspire-converter/src/main/java/ro/negru/mihai/webinspireconverter/WebInspireConverterApplication.java
+++ web-inspire-converter/src/main/java/ro/negru/mihai/webinspireconverter/WebInspireConverterApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebInspireConverterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebInspireConverterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebInspireConverterApplication.class, args);
+    }
 
 }
