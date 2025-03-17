6f528783b01628baa0c6e684ce0641deec9309d1
mihai-negru <determinant289@gmail.com>
2025-03-17 12:06:27 +0200

    Init projects

---
+++ web-inspire-converter/src/main/java/ro/negru/mihai/webinspireconverter/WebInspireConverterApplication.java
@@ -0,0 +1,13 @@
+package ro.negru.mihai.webinspireconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebInspireConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebInspireConverterApplication.class, args);
+	}
+
+}
---
+++ web-inspire-converter/src/test/java/ro/negru/mihai/webinspireconverter/WebInspireConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.negru.mihai.webinspireconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebInspireConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
