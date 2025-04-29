66272adb353f24eb326c5a8d4d154122a33953eb
mihai-negru <determinant289@gmail.com>
2025-04-29 11:57:19 +0300

    Small flink test this should be ignored

--- flink-inspire-transformer/src/main/java/ro/negru/mihai/DataStreamJob.java
+++ flink-inspire-transformer/src/main/java/ro/negru/mihai/DataStreamJob.java
@@ -18,48 +18,62 @@
 
 package ro.negru.mihai;
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
-/**
- * Skeleton for a Flink DataStream Job.
- *
- * <p>For a tutorial how to write a Flink application, check the
- * tutorials and examples on the <a href="https://flink.apache.org">Flink Website</a>.
- *
- * <p>To package your application into a JAR file for execution, run
- * 'mvn clean package' on the command line.
- *
- * <p>If you change the name of the main class (with the public static void main(String[] args))
- * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
- */
+import java.util.regex.Pattern;
+
 public class DataStreamJob {
 
 	public static void main(String[] args) throws Exception {
-		// Sets up the execution environment, which is the main entry point
-		// to building Flink applications.
+
+		KafkaTopicCreator.createTopicIfNotExist("broker:9092", "test-topic");
+
+
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		/*
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.fromSequence(1, 10);
-		 *
-		 * then, transform the resulting DataStream<Long> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.window()
-		 * 	.process()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide:
-		 *
-		 * https://nightlies.apache.org/flink/flink-docs-stable/
-		 *
-		 */
+		final KafkaSource<String> source = KafkaSource.<String>builder()
+				.setBootstrapServers("broker:9092")
+				.setTopicPattern(Pattern.compile("test-topic.*"))
+				.setGroupId("group-id")
+				.setStartingOffsets(OffsetsInitializer.earliest())
+				.setValueOnlyDeserializer(new SimpleStringSchema())
+				.setProperty("partition.discovery.interval.ms", "10000")
+				.setProperty("allow.auto.create.topics", "true")
+				.build();
 
-		// Execute program, beginning computation.
-		env.execute("Flink Java API Skeleton");
+		DataStream<String> data = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
+
+		DataStream<String> sinker = data.flatMap((String str, Collector<Integer> out) -> {
+			// Split by one or more whitespace characters
+			for (String token : str.split("\\s+")) {
+				try {
+					out.collect(Integer.parseInt(token));
+				} catch (NumberFormatException e) {
+					// Optionally log or handle tokens that cannot be parsed
+				}
+			}
+		}).returns(Types.INT).keyBy(num -> 0).reduce(Integer::sum).map(Object::toString);
+
+		KafkaSink<String> sink = KafkaSink.<String>builder()
+				.setBootstrapServers("broker:9092")
+				.setRecordSerializer(KafkaRecordSerializationSchema.builder()
+						.setTopic("test-response")
+						.setValueSerializationSchema(new SimpleStringSchema())
+						.build()
+				)
+				.build();
+
+		sinker.sinkTo(sink);
+
+		env.execute("Simple Kafka Source");
 	}
 }
