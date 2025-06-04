dc478b9ec239e8225af7b3fd0e55178b4053732f
mihai-negru <determinant289@gmail.com>
2025-06-04 15:18:32 +0300

    Optimize imports

--- kafka-inspire-validator-connector/src/main/java/ro/negru/mihai/kafkainspirevalidatorconnector/service/TestJobService.java
+++ kafka-inspire-validator-connector/src/main/java/ro/negru/mihai/kafkainspirevalidatorconnector/service/TestJobService.java
@@ -13,8 +13,6 @@ import ro.negru.mihai.kafkainspirevalidatorconnector.status.ValidatorTestRespons
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 @Service
 public class TestJobService {
