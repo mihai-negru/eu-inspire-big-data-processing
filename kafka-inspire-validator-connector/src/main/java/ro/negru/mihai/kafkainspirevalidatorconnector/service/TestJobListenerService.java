7c46c53c7e3c9f8f5828e5b93b3bc4263555d0ed
mihai-negru <determinant289@gmail.com>
2025-06-05 22:20:29 +0300

    Modified the kakfa deserializer to a simple string and convert to schema inside the submitter

--- kafka-inspire-validator-connector/src/main/java/ro/negru/mihai/kafkainspirevalidatorconnector/service/TestJobListenerService.java
+++ kafka-inspire-validator-connector/src/main/java/ro/negru/mihai/kafkainspirevalidatorconnector/service/TestJobListenerService.java
@@ -1,6 +1,9 @@
 package ro.negru.mihai.kafkainspirevalidatorconnector.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import ro.negru.mihai.kafkainspirevalidatorconnector.dto.TestRequest;
@@ -8,14 +11,22 @@ import ro.negru.mihai.kafkainspirevalidatorconnector.dto.TestRequest;
 @Component
 @RequiredArgsConstructor
 public class TestJobListenerService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestJobListenerService.class);
+
     private final TestJobService testJobService;
+    private final ObjectMapper objectMapper;
 
     @KafkaListener(
             topics = "${topics.input}",
             groupId = "${spring.kafka.consumer.group-id}",
             containerFactory = "kafkaListenerContainerFactory"
     )
-    public void onTestRequest(TestRequest req) {
-        testJobService.submitNewTestJob(req);
+    public void onTestRequest(final String json) {
+        try {
+            final TestRequest testRequest = objectMapper.readValue(json, TestRequest.class);
+            testJobService.submitNewTestJob(testRequest);
+        } catch (Exception e) {
+            LOGGER.error("Could not parse the json object", e);
+        }
     }
 }
